30823a703792867ee5f2a859c58544b1d5b9cce8
Mostlyharmless26 <Cameronrossjohnson@gmail.com>
2017-04-02 23:51:44 -0500

    Added minimax
    Reworked some structure and got minimax working

--- chess_ai.py
+++ chess_ai.py
@@ -4,18 +4,61 @@ from random import randint, shuffle
 
 class ChessAi:
 
-    def __init__(self):
+    def __init__(self, eval_function):
+        self.eval_function = eval_function
         pass
 
-    def get_move(self, board):
-        moves = self.evaluate_board_random(board)
-        return moves[0]
+    def get_move(self, player, board):
+        move = self.get_strongest_move(player, board)
+        return move
 
-    def evaluate_board_random(self, board):
-        moves = list(board.legal_moves)
-        shuffle(moves)
-        return moves
+    def minimax(self, depth, is_max_player, board):
+        if depth == 0:
+            return self.eval_function(board) * (1 if is_max_player else -1)
+        if is_max_player:
+            best_score = -9999
+            for move in board.legal_moves:
+                board.push(move)
+                best_score = max(best_score, self.minimax(depth - 1, not is_max_player, board))
+                board.pop()
+            return best_score
+        else:
+            best_score = 9999
+            for move in board.legal_moves:
+                board.push(move)
+                best_score = min(best_score, self.minimax(depth - 1, not is_max_player, board))
+                board.pop()
+            return best_score
 
-    def evaluate_board_simple_count(self, board):
+    def get_strongest_move(self, player, board):
         moves = list(board.legal_moves)
-        return moves
+        ranked_moves = []
+        for move in moves:
+            board.push(move)
+            score = self.minimax(4, True, board)
+            ranked_moves.append((score, move))
+            board.pop()
+        sorted_moves = sorted(ranked_moves, key=lambda i: i[0], reverse=True)
+        return [a[1] for a in sorted_moves][0]
+
+
+def evaluate_board_random(board):
+    return randint(-5, 5)
+
+
+def evaluate_board_simple_count(board):
+    return sum([
+        len(board.pieces(chess.PAWN, chess.WHITE)) * 1,
+        len(board.pieces(chess.PAWN, chess.BLACK)) * -1,
+        len(board.pieces(chess.KNIGHT, chess.WHITE)) * 3,
+        len(board.pieces(chess.KNIGHT, chess.BLACK)) * -3,
+        len(board.pieces(chess.BISHOP, chess.WHITE)) * 3,
+        len(board.pieces(chess.BISHOP, chess.BLACK)) * -3,
+        len(board.pieces(chess.ROOK, chess.WHITE)) * 5,
+        len(board.pieces(chess.ROOK, chess.BLACK)) * -5,
+        len(board.pieces(chess.QUEEN, chess.WHITE)) * 9,
+        len(board.pieces(chess.QUEEN, chess.BLACK)) * -9,
+        len(board.pieces(chess.KING, chess.WHITE)) * 9000,
+        len(board.pieces(chess.KING, chess.BLACK)) * -9000
+    ])
+
--- chess_player.py
+++ chess_player.py
@@ -1,25 +1,30 @@
 import chess
-from chess_ai import ChessAi
+from chess_ai import ChessAi, evaluate_board_random, evaluate_board_simple_count
 
-def play_game():
+def play_game(debug):
 
-    ai = ChessAi()
+    random_ai = ChessAi(evaluate_board_random)
+    count_ai = ChessAi(evaluate_board_simple_count)
     board = chess.Board()
     white_to_move = True
-
     while not board.is_game_over():
-        print("%s\n" % board)
+        if debug:
+            print("%s\n" % board)
         if white_to_move:
-            next_move = ai.get_move(board)
+            next_move = random_ai.get_move(chess.WHITE, board)
         else:
-            next_move = ai.get_move(board)
+            next_move = count_ai.get_move(chess.BLACK, board)
         if next_move in board.legal_moves:
             board.push(next_move)
             white_to_move = not white_to_move
     if board.is_checkmate():
-        print("Black wins!" if white_to_move else "White wins!")
+        if debug:
+            print("Black wins!" if white_to_move else "White wins!")
     else:
-        print("It's a Draw!")
+        if debug:
+            print("It's a Draw!")
+    print("%s moves total" % board.fullmove_number)
+    return board.result()
 
 
 def get_human_move(board):
@@ -28,7 +33,6 @@ def get_human_move(board):
         print("Invalid move. Try again.")
         move = get_move_from_string(input("Enter a Valid Move: "), board)
     return move
-
 
 def get_move_from_string(istr, board):
     try:
@@ -40,5 +44,20 @@ def get_move_from_string(istr, board):
     except ValueError:
         return False
 
+
+def play_one():
+    play_game(True)
+
+
+
+def play_25():
+    results = []
+    for i in range(0, 25):
+        results.append(play_game(False))
+    print("%s White Wins" % len([a for a in results if a == "1-0"]))
+    print("%s Black Wins" % len([a for a in results if a == "0-1"]))
+    print("%s Draw" % len([a for a in results if a == "1/2-1/2"]))
+
+
 if __name__ == '__main__':
-    play_game()
+    print(play_one())
