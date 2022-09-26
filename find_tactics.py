f24d4c639b74e2c8782129d7f5fc7e709a87ea75
Cameron Johnson <cameronrossjohnson@gmail.com>
2022-09-26 12:41:26 -0500

    Fixed tactics

--- find_tactics.py
+++ find_tactics.py
@@ -13,16 +13,17 @@ def find_tactics(lower_difficulty, higher_difficulty, game):
     cur_node = game.end()
     while cur_node.parent is not None:
         prev_node = cur_node.parent
-        cur_move = str(cur_node.move)
+        cur_move = cur_node.move
         board = prev_node.board()
         weak_evals = lower_player.rank_moves(board)
         high_evals = higher_player.rank_moves(board)
 
         weak_eval = [move for move in weak_evals if move[1] == cur_move][0][0]
         high_eval = [move for move in high_evals if move[1] == cur_move][0][0]
+        print(cur_move, high_eval, weak_eval)
 
         difference = high_eval - weak_eval
-        if difference > 1:
+        if abs(difference) > 1:
             if board.turn == 1:
                 high_eval = high_eval * -1
                 weak_eval = weak_eval * -1
@@ -33,10 +34,11 @@ def find_tactics(lower_difficulty, higher_difficulty, game):
             actually_winning = high_eval > 0 and weak_eval <= 0
             actually_losing = high_eval < 0 and weak_eval >= 0
             actually_tied = high_eval == 0 and not weak_eval == 0
-            board.push(cur_node.move)
 
             print(board.fen(), ' Difference: ', difference)
-            print(high_eval, weak_eval, cur_move)
+
+            board.push(cur_node.move)
+
             print(winning_by_more and 'Winning by More')
             print(losing_by_more and 'Losing by More')
             print(winning_by_less and 'Winning by Less')
@@ -47,6 +49,6 @@ def find_tactics(lower_difficulty, higher_difficulty, game):
         cur_node = prev_node
 
 if __name__ == '__main__':
-    game_name = 'Cameron Chess AI- 1663996709.7744613.pgn'
+    game_name = 'Cameron Chess AI- 1664037023.208234.pgn'
     game = chess.pgn.read_game(open(join('game_results', game_name)))
-    find_tactics(2, 4, game)
+    find_tactics(3, 4, game)
